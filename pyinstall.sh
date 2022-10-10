168706fa90f9f85e545e05c64d56e3f705342210
子木仄言 <34052991+97xyg@users.noreply.github.com>
2022-10-10 20:34:07 +0800

    Update pyinstall.sh

--- pyinstall.sh
+++ pyinstall.sh
@@ -2,14 +2,14 @@
 yum install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gcc make -y
 yum install libffi-devel -y
 cd ~
-wget http://cdn.npm.taobao.org/dist/python/3.6.5/Python-3.6.5.tgz
-tar -zxvf Python-3.6.5.tgz
-cd Python-3.6.5
+wget http://cdn.npmmirror.com/binaries/python/3.8.5/Python-3.8.5.tgz
+tar -zxvf Python-3.8.5.tgz
+cd Python-3.8.5
 ./configure prefix=/usr/local/python3
 make && make install
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/bin/pip3
 cd ~
-rm -rf Python-3.6.5 Python-3.6.5.tgz
+rm -rf Python-3.8.5 Python-3.8.5.tgz
 echo "脚本执行完成！"
 echo "输入python3 -V 验证python3是否安装成功，输入pip3 -V 验证匹配是否成功"
